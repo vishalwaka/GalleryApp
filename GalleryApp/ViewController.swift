cb5000d99ef697acf77727cc9d024c36841a7eec
user136229 <user136229@103-228-152-190.moboware.com>
2018-02-12 00:18:58 +0530

    Initial Commit

---
+++ GalleryApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GalleryApp
+//
+//  Created by user136229 on 2/12/18.
+//  Copyright © 2018 Vishal Madheshia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
